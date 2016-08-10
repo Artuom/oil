8e09e29ac8f2fccba163d141210ec196ad31c8aa
Artuom <aprotasenya@gmail.com>
2016-08-10 16:01:32 +0300

    changes in os_period v1

--- os_period.py
+++ os_period.py
@@ -6,8 +6,9 @@ program = ['ussd_life.py', 'ussd_mts.py', 'ussd_velcom.py']
 prid_dict = {}
 for pr in program:
     try:
-        output = commands.getoutput("ps -f|grep " + pr)
-        proginfo = string.split(output)
+        output = commands.getoutput("ps - ef | grep " + pr)
+        my_list = [my_str for my_str in output.split('\n') if 'python' in my_str]
+        proginfo = string.split(my_list[0])
         process_id = proginfo[1]
         starttime = proginfo[4]
         prid_dict[pr] = process_id
