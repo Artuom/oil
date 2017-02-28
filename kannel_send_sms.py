ecb452616fc2c0558b9e970fce0660d67cc3e62c
Artuom <aprotasenya@gmail.com>
2017-02-28 14:29:04 +0300

    added changes with sms sending v4

--- kannel_send_sms.py
+++ kannel_send_sms.py
@@ -8,7 +8,7 @@ def send_sms(msisdn, text, network):
         text = text.encode('utf-8')
         print text
         r = requests.get("http://localhost:13003/cgi-bin/sendsms?smsc=life&username=user_life&"
-                        "password=usrlife&coding=2&to={}&text={}&charset=UTF-8".format(msisdn, text))
+                        "password=usrlife&coding=2&to={}&text='{}'&charset=UTF-8".format(msisdn, text))
         return r
     except Exception as err:
         print err
